d7eca62b399dc787dc61278059458f9305af7be3
vanstee <patrick@vanstee.me>
2015-11-14 00:07:45 -0500

    Use macros and helpers to clean things up a bit

---
+++ lib/abnf/operators.ex
@@ -0,0 +1,118 @@
+defmodule ABNF.Operators do
+  defmacro defrule(name, do: block) do
+    quote do
+      def parse(unquote(name)) do
+        fn input ->
+          case unquote(block).(input) do
+            :error ->
+              :error
+            [{:literal, preview, []}] ->
+              [{unquote(name), preview, []}]
+            children ->
+              [{unquote(name), preview(children), children}]
+          end
+        end
+      end
+    end
+  end
+
+  def literal(element) do
+    fn input ->
+      literal(element, input)
+    end
+  end
+
+  def literal([element], input) do
+    case input do
+      [^element|_] ->
+        [{:literal, to_string([element]), []}]
+      _ ->
+        :error
+    end
+  end
+
+  def concatenate(elements) do
+    fn input ->
+      concatenate(elements, [], input)
+    end
+  end
+
+  defp concatenate([], acc, '') do
+    acc
+    |> List.flatten
+    |> Enum.reverse
+  end
+
+  defp concatenate([element|elements], acc, input) do
+    case element.(input) do
+      :error ->
+        :error
+      children ->
+        input = advance(children, input)
+        children = Enum.reverse(children)
+        concatenate(elements, [children|acc], input)
+    end
+  end
+
+  def alternate(elements) do
+    fn input ->
+      alternate(elements, input)
+    end
+  end
+
+  defp alternate([], _input) do
+    :error
+  end
+
+  defp alternate([element|elements], input) do
+    case element.(input) do
+      :error ->
+        alternate(elements, input)
+      child ->
+        child
+    end
+  end
+
+  def repeat(min, max, element) do
+    fn input ->
+      repeat(min, max, element, 0, [], input)
+    end
+  end
+
+  defp repeat(_min, max, _element, max, acc, _input) do
+    acc
+    |> List.flatten
+    |> Enum.reverse
+  end
+
+  defp repeat(min, max, element, count, acc, input) do
+    case element.(input) do
+      :error when count >= min ->
+        repeat(min, max, element, max, acc, input)
+      :error ->
+        :error
+      children ->
+        input = advance(children, input)
+        children = Enum.reverse(children)
+        repeat(min, max, element, count + 1, [children|acc], input)
+    end
+  end
+
+  def preview(children) do
+    children
+    |> Enum.map(fn {_, preview, _} -> preview end)
+    |> Enum.join
+  end
+
+  def advance(children, input) do
+    children
+    |> preview
+    |> String.to_char_list
+    |> string_subtract(input)
+  end
+
+  def string_subtract([], string), do: string
+  def string_subtract([h|substring], [h|string]) do
+    string_subtract(substring, string)
+  end
+end
--- lib/abnf/rfc5234.ex
+++ lib/abnf/rfc5234.ex
@@ -1,4 +1,6 @@
 defmodule ABNF.RFC5234 do
+  import ABNF.Operators
+
   def parse(rule, input) when is_binary(input) do
     parse(rule, String.to_char_list(input))
   end
@@ -7,195 +9,73 @@ defmodule ABNF.RFC5234 do
     parse(rule).(input)
   end
 
+  defrule :"c-wsp" do
+    alternate([
+      parse(:wsp),
+      concatenate([
+        parse(:"c-nl"),
+        parse(:wsp)
+      ])
+    ])
+  end
+
+  defrule :"c-nl" do
+    alternate([
+      parse(:comment),
+      parse(:crlf)
+    ])
+  end
+
   # NOTE: alternate's arg order reversed
-  def parse(:repeat) do
-    fn input ->
-      results = alternate([
-        concatenate([
-          repeat(0, :infinity, parse(:digit)),
-          fn
-            [?*|_] ->
-              [{:literal, "*", []}]
-            _ ->
-              :error
-          end,
-          repeat(0, :infinity, parse(:digit))
-        ]),
-        repeat(1, :infinity, parse(:digit))
-      ]).(input)
-
-      preview = results
-      |> Enum.map(fn {_, preview, _} -> preview end)
-      |> Enum.join
-
-      [{:repeat, preview, results}]
-    end
+  defrule :repeat do
+    alternate([
+      concatenate([
+        repeat(1, :infinity, parse(:digit)),
+        literal('*'),
+        repeat(0, :infinity, parse(:digit))
+      ]),
+      repeat(1, :infinity, parse(:digit))
+    ])
   end
 
-  def parse(:bit) do
-    fn input ->
-      alternate([
-        fn
-          [?0|_] ->
-            [{:bit, "0", []}]
-          _ ->
-            :error
-        end,
-        fn
-          [?1|_] ->
-            [{:bit, "1", []}]
-          _ ->
-            :error
-        end
-      ]).(input)
-    end
+  defrule :bit do
+    alternate([
+      literal('0'),
+      literal('1')
+    ])
   end
 
-  def parse(:crlf) do
-    fn input ->
-      results = concatenate([
-        parse(:cr),
-        parse(:lf)
-      ]).(input)
-
-      preview = results
-      |> Enum.map(fn {_, preview, _} -> preview end)
-      |> Enum.join
-
-      [{:crlf, preview, results}]
-    end
+  defrule :crlf do
+    concatenate([
+      parse(:cr),
+      parse(:lf)
+    ])
   end
 
-  def parse(:cr) do
-    fn
-      [?\r|_] ->
-        [{:cr, "\r", []}]
-      _ ->
-        :error
-    end
+  defrule :cr do
+    literal('\r')
   end
 
-  def parse(:lf) do
-    fn
-      [?\n|_] ->
-        [{:lf, "\n", []}]
-      _ ->
-        :error
-    end
+  defrule :lf do
+    literal('\n')
   end
 
-  def parse(:digit) do
-    fn
-      [?0|_] ->
-        [{:digit, "0", []}]
-      [?1|_] ->
-        [{:digit, "1", []}]
-      [?2|_] ->
-        [{:digit, "2", []}]
-      [?3|_] ->
-        [{:digit, "3", []}]
-      [?4|_] ->
-        [{:digit, "4", []}]
-      [?5|_] ->
-        [{:digit, "5", []}]
-      [?6|_] ->
-        [{:digit, "6", []}]
-      [?7|_] ->
-        [{:digit, "7", []}]
-      [?8|_] ->
-        [{:digit, "8", []}]
-      [?9|_] ->
-        [{:digit, "9", []}]
-      _ ->
-        :error
-    end
+  defrule :digit do
+    alternate([
+      literal('0'),
+      literal('1'),
+      literal('2'),
+      literal('3'),
+      literal('4'),
+      literal('5'),
+      literal('6'),
+      literal('7'),
+      literal('8'),
+      literal('9')
+    ])
   end
 
-  def parse(:dquote) do
-    fn
-      [?\"|_] ->
-        [{:dquote, "\"", []}]
-      _ ->
-        :error
-    end
-  end
-
-  defp concatenate(elements) do
-    fn input ->
-      concatenate(elements, [], input)
-    end
-  end
-
-  defp concatenate([], acc, '') do
-    acc
-    |> List.flatten
-    |> Enum.reverse
-  end
-
-  defp concatenate([element|elements], acc, input) do
-    case element.(input) do
-      :error ->
-        :error
-      results ->
-        preview = results
-        |> Enum.map(fn {_, preview, _} -> preview end)
-        |> Enum.join
-
-        input = to_string(input)
-        ["", input] = String.split(input, preview, parts: 2)
-        input = String.to_char_list(input)
-
-        concatenate(elements, [Enum.reverse(results)|acc], input)
-    end
-  end
-
-  defp alternate(elements) do
-    fn input ->
-      alternate(elements, input)
-    end
-  end
-
-  defp alternate([], _input) do
-    :error
-  end
-
-  defp alternate([element|elements], input) do
-    case element.(input) do
-      :error ->
-        alternate(elements, input)
-      result ->
-        result
-    end
-  end
-
-  defp repeat(min, max, element) do
-    fn input ->
-      repeat(min, max, element, 0, [], input)
-    end
-  end
-
-  defp repeat(_min, max, _element, max, acc, _input) do
-    acc
-    |> List.flatten
-    |> Enum.reverse
-  end
-
-  defp repeat(min, max, element, count, acc, input) do
-    case element.(input) do
-      :error when count >= min ->
-        repeat(min, max, element, max, acc, input)
-      :error ->
-        :error
-      results ->
-        preview = results
-        |> Enum.map(fn {_, preview, _} -> preview end)
-        |> Enum.join
-
-        input = to_string(input)
-        ["", input] = String.split(input, preview, parts: 2)
-        input = String.to_char_list(input)
-
-        repeat(min, max, element, count + 1, [Enum.reverse(results)|acc], input)
-    end
+  defrule :dquote do
+    literal('"')
   end
 end
